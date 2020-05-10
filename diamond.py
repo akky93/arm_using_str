441db8a78108ffec5577d4c3dfb29d1d5953dcf9
akky93 <akkykumar19@gmail.com>
2020-05-11 01:03:38 +0530

    Update diamond.py

--- diamond.py
+++ diamond.py
@@ -4,7 +4,5 @@ for i in range(0,7):
         if i+n==3 or n-i==3 or i-n==3 or i+n==9:
             print('*',end=' ')
         else:
-            print(end='  ')
-    
+            print(end='  ') 
     print()
-    
